020f1ce0fb147aca86810bd56263af018644742e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-20 12:20:11 +0200

    firebase auth initialisé ajout de firebase et firebase/auth et des configs

---
+++ configs/firebase.ts
@@ -0,0 +1,18 @@
+import {initializeApp, initializeServerApp, type FirebaseOptions } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig: FirebaseOptions = {
+    // @ts-expect-error
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId:import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_API_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const auth = getAuth(app);
+
+  export { auth };
