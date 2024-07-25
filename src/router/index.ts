63467b56f43744878ff00e81a3353ff727ec92f1
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-25 22:05:38 +0200

    reprendre

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
+import { auth } from '@/configs/firebase'
 import LoginView from '@/views/LoginView.vue'
 import TodoView from '@/views/TodoView.vue'
 import { useAuth } from '@vueuse/firebase'
 import { createRouter, createWebHistory } from 'vue-router'
+import  Register  from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -39,7 +41,7 @@ const router = createRouter({
 
 // Les gardients de navigation
 router.beforeEach((to, from) => {
-  const {isAuthenticated, user} = useAuth(auth);
+  const {isAuthenticated} = useAuth(auth);
   if (!isAuthenticated && (to.name !== 'login' && to.name !== 'register')) {
     return {name: 'login'}
   }
